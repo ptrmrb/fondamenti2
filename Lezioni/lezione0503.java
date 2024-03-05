a43365e7856256fbf924cdc4350c70e574dc2c4f
ptrmrb <161971499+ptrmrb@users.noreply.github.com>
2024-03-05 11:11:17 +0000

    gg

--- Lezioni/lezione0503.java
+++ Lezioni/lezione0503.java
@@ -1,19 +1,18 @@
 package Lezioni;
 
+import terminale.*; // * vuol dire importa tutte le classi presenti nel package termiale 
+
 public class lezione0503 
 {
     public static void main(String[] args)
     {
-        
+        String nome  = Terminale.richiediStringa("Come ti chiami?");
+        String risposta = " Ciao, " + nome + "!";
+        Terminale.stampa(risposta);
 
     }
     
-    public static int metodoEsempio(int x, int y){
-        return 10;
-    }
 
-    public static boolean metodoEsempio(int x, boolean y){
-        return false;
     }
 }
 
