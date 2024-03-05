29ac477b734551b4a2cc62190d4214759d7ff4bf
ptrmrb <161971499+ptrmrb@users.noreply.github.com>
2024-03-05 13:49:16 +0000

    gg

--- Lezioni/lezione0503.java
+++ Lezioni/lezione0503.java
@@ -1,22 +1,24 @@
 package Lezioni;
 
-import terminale.*; // * vuol dire importa tutte le classi presenti nel package termiale 
+//import terminale.*; // * vuol dire importa tutte le classi presenti nel package termiale 
 
 public class lezione0503 
 {
     public static void main(String[] args)
     {
-        String nome  = Terminale.richiediStringa("Come ti chiami?");
+        /*String nome  = Terminale.richiediStringa("Come ti chiami?");
         String risposta = " Ciao, " + nome + "!";
         Terminale.stampa(risposta);
 
         double valore = Math.PI;
         Terminale.stampa(valore);
-        Math.sqrt(valore);
+        Math.sqrt(valore);*/
 
         int[] a = new int[] {1,0,-2, 3, 5, -1, 3};
-        int positivi = selezionaPositivi1(a);
-
+        int[] positivi = selezionaPositivi2(a);
+        
+        System.out.println("");
+        System.out.println(positivi);
     }
     
     public static int[] selezionaPositivi1( int[] arr ) 
@@ -40,27 +42,27 @@ public class lezione0503
             }
 
         return ret;
-    
+        }
+
     public static int[] selezionaPositivi2( int[] arr2 )
     { // quando creo una struttura d'appoggio, il suo nome è buffer
 
         int[] buf = new int[arr2.length];
-        int posizioneLibera1 = 0;
+        int posizioneLibera = 0;
 
         for(int n : arr2 )
             if (n > 0)
             {
-                buf[posizioneLibera1] = n;
-                posizioneLibera1++;
+                buf[posizioneLibera] = n;
+                posizioneLibera++;
             }
 
-        int [] ret = new int[posizioneLibera1];
-        for( int i = 0; i < posizioneLibera1; i++)
+        int[] ret = new int[posizioneLibera];
+        for( int i = 0; i < posizioneLibera; i++)
             ret[i] = buf[i];
 
         return ret;
 
-    }
     }
 }
 
