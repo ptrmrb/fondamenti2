b5332723c9cdf5422b9f377ef1dfb46265c7f6fe
ptrmrb <161971499+ptrmrb@users.noreply.github.com>
2024-03-05 10:48:19 +0000

    lezione in aula

---
+++ Lezioni/lezione0503.java
@@ -0,0 +1,19 @@
+package Lezioni;
+
+public class lezione0503 
+{
+    public static void main(String[] args)
+    {
+        
+
+    }
+    
+    public static int metodoEsempio(int x, int y){
+        return 10;
+    }
+
+    public static boolean metodoEsempio(int x, boolean y){
+        return false;
+    }
+}
+
--- lezione0503.java
+++
@@ -1,6 +0,0 @@
-public class lezione0503{
-    public static void main(String[] args){
-        System.out.println("ciao");
-        System.out.println("Hello");
-    }
-}
