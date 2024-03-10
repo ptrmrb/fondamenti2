256b3cca94acee0e984a6b897455ab926cd970d2
ptrmrb <161971499+ptrmrb@users.noreply.github.com>
2024-03-10 21:06:51 +0000

    commit

--- lezioni/Persona.java
+++ lezioni/Persona.java
@@ -1,13 +1,41 @@
 package lezioni;
 
-public class Persona {
+public class Persona 
+    {
 
-public String nome;
-public String cognome; 
-public int annoNascita;
+    public String nome;
+    public String cognome; 
+    public int annoNascita;
 
 
-    public String toString(){
-        return nome + " " + cognome + " nato nel " + annoNascita;
+    public String toString()
+    {   return nome + " " + cognome + " nato nel " + annoNascita;
     }
-}
+
+    // mutator method 
+
+    public void setNome(String nome)
+        {   this.nome = nome; // quindi nel main scriveremo pi.setNome(“Mario”)
+        }
+    public void setCognome(String cognome)
+        {   this.cognome = cognome;
+        }
+    public void setAnnoNascita(int annoNascita)
+        {   this.annoNascita = annoNascita;
+        }
+    
+    // accessor method 
+    public String getNome()
+        {   return nome; 
+        }
+
+
+    // constructor method
+
+    public void persona ( String nome, String cognome, int annoNascita ) {
+        setNome(nome);
+        setCognome(cognome);
+        setAnnoNascita(annoNascita); // sempre con l’iniziale minuscola. 
+        }
+    
+    }
