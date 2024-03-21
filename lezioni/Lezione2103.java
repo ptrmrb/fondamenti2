82a08fe2d713406ec1393d8f382f8ca36421e0cb
ptrmrb <161971499+ptrmrb@users.noreply.github.com>
2024-03-21 11:42:40 +0100

    ciao

---
+++ lezioni/Lezione2103.java
@@ -0,0 +1,25 @@
+public class Lezione2103{
+    public static void main (String[] args){
+        int eta = Terminale.richiediInt("Età: ");
+
+        // CONDITIONAL EXPRESSION
+        //TERNARY OPERATOR
+
+        // dichiaro una variabile s e la inizializzo direttamente
+
+        String s = " ";
+        if ( eta >= 18)
+            s = "maggiorenne";
+        else
+            s = "minorenne";
+
+        Terminale.stampa(s);
+        Terminale.stampa(s); 
+
+
+
+        String s = (eta >= 18 ? "maggiorenne" : " minorenne");
+
+        Terminale.stampa(s);
+    }
+}
