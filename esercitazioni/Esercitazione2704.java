435b1591e7d5df9c9296b7857894a2ecfaffaf46
ptrmrb <161971499+ptrmrb@users.noreply.github.com>
2024-03-27 10:54:44 +0100

    co

---
+++ esercitazioni/Esercitazione2704.java
@@ -0,0 +1,24 @@
+package esercitazioni;
+
+public class Esercitazione2003 
+{
+    /* Si scriva un metodo filtraMatrice che riceve in input una
+    matrice di interi e restituisce un array contenente tutti gli 
+    elementi presenti sulle righe di indice pari che contengono solo
+    elementi dispari*/
+
+    public static int[] filtraMatrice (int[][] m)
+    {
+        int[] ret = new int [dim]; // utilizzare quando non conosciamo la dimensione di qualche ogetto
+        int dim = 0;
+        int numRigheOk = 0;
+
+        for ( int i = 0; i<m.length; i+=2) // incrementiamo di 2 in modo tale da controllare le righe pari, ovvero il passo
+        {
+            if ( rigaTuttiDispari(m,i) )
+                numRigheOk ++; 
+        }
+        dim = numRigheOk * m[0].length;
+        int[] ris = new int[dim]; 
+    }
+}
