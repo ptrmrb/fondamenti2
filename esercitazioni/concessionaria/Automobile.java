88603f117a042beb287ba3043db81d9cc809f3a1
ptrmrb <pietromirab04@gmail.com>
2024-04-13 12:20:58 +0200

    ca

--- esercitazioni/concessionaria/Automobile.java
+++ esercitazioni/concessionaria/Automobile.java
@@ -1,65 +1,68 @@
-package concessionaria; 
+/*
+ * Progettare e implementare una classe Automobile che ne rappresenti modello,
+ * colore, casa produttrice, cilindrata, prezzo e numero di telaio.
+ * Il numero di telaio identifica un'automobile. 
+ * 
+ */
 
-public class Automobile 
-{
-    private String modello; 
-    private String colore; 
-    private String casaProduttrice;
-    private int cilindrata;
-    private float prezzo;
-    private String numeroTelaio; // ID
+ package concessionaria;
 
-    /* 
-    Progettare e implementare una classe automobile
-    
-    
-    */ 
-
-   public Automobile (String modello, String colore, String casaProduttrice, int cilindrata, float prezzo, String numeroTelaio) 
-   {    
-        this.modello = modello;
-        this.colore = colore;
-        this.casaProduttrice = casaProduttrice;
-        this.cilindrata = cilindrata;
-        this.prezzo = prezzo;
-        this.numeroTelaio = numeroTelaio; 
-   }
-
-   public String getModello(){
-    return modello;
-   }
-
-   public String getColore(){
-    return colore;
-   }
-
-   public String getCasaProduttrice(){
-    return casaProduttrice;
-   }
-
-   public String getCilindrata(){
-    return cilindrata;
-   }
-
-   public String getPrezzo(){
-    return prezzo;
-   }
-
-   public String getNumeroTelaio(){
-    return numeroTelaio;
-   }
-
-    @Override
-   public String equals(Object o)
-   {    
-        if ( o == null )
-            return false; 
-        if ( o == this )
-            return true;
-        if(!(o instanceof Automobile )) return false;
-        Automobile a = ( Automobile ) o; 
-        return a.numeroTelaio.equals(this.numeroTelaio);
-   }
+ public class Automobile // La classe genera oggetti immutabili 
+ {	private String modello; // es. "Punto"
+     private String colore; // es. "Nero"
+     private String casaProduttrice; // es. "Fiat"
+     private int cilindrata; // es. 1300
+     private float prezzo; // es. 12000
+     private String numTelaio; // es. "ABC123456789"
+     
+     public Automobile(String modello, String colore, String casaProduttrice, int cilindrata, float prezzo, String numTelaio) {
+         this.modello = modello;
+         this.colore = colore;
+         this.casaProduttrice = casaProduttrice;
+         this.cilindrata = cilindrata;
+         this.prezzo = prezzo;
+         this.numTelaio = numTelaio;
+     }
+ 
+     public String getModello() 
+     {	return modello;
+     }
+ 
+     public String getColore() 
+     {	return colore;
+     }
+ 
+     public String getCasaProduttrice() 
+     {	return casaProduttrice;
+     }
+ 
+     public int getCilindrata() 
+     {	return cilindrata;
+     }
+ 
+     public float getPrezzo() 
+     {	return prezzo;
+     }
+ 
+     public String getNumTelaio() 
+     {	return numTelaio;
+     }
+     
+ 
+     public String toString() 
+     {	return casaProduttrice + " " + modello + ", telaio " + numTelaio;
+     }
+     
+     public boolean equals(Object x) 
+     {	if(x == null) 
+             return false;
+         if(this == x) 
+             return true;
+         if(!(x instanceof Automobile)) 
+             return false;
+         Automobile a = (Automobile) x;
+         return numTelaio.equals(a.numTelaio);
+     }
 
    
 
--- esercitazioni/concessionaria/Concessionaria.java
+++ esercitazioni/concessionaria/Concessionaria.java
@@ -1,151 +1,156 @@
-package concessionaria;
+/*
+ * Progettare e implementare una classe Concessionaria tenendo conto delle seguenti
+ * specifiche:
+ * 		-	La concessionaria contiene un magazzino (cio� una lista di automobili).
+ * 		-	La classe deve contenere metodi per:
+ * 				-	aggiungere un'auto al magazzino;
+ * 				-	rimuovere un'auto dal magazzino;
+ * 				-	stampare tutte le auto;
+ * 				-	calcolare la somma dei prezzi di tutte le auto;
+ * 				-	trovare la casa produttrice pi� ricorrente;
+ * 				-	trovare l'auto con la cilindrata pi� bassa tra quelle con un prezzo
+ * 					inferiore a 10.000 euro;
+ * 				-	trovare l'auto con la cilindrata pi� alta tra quelle con un prezzo
+ * 					superiore alla media dei prezzi delle auto;
+ * 				-	trovare le case produttrici che producono solo auto con un prezzo
+ * 					inferiore alla media dei prezzi delle auto;
+ * 				-	trovare le case produttrici che producono solo auto con la stessa
+ * 					cilindrata.
+ */
 
-import java.util.ArrayList;
-import terminale.*;
-
-public class Concessionaria{
-
-    private ArrayList<Automobile> magazzino;
-
-    // costr. default
-    public Concessionaria(Concessionaria c)
-    {
-        magazzino = new ArrayList<>();
-        for (Automobile a: c.magazzino){
-            Automobile a1 = new Automobile(a1.getModello ... );
-
-        }
-    }
-
-    public void stampaAutomobili()
-    {
-        for(Automobile a: magazzino)
-            Terminale.stampa(a);
-
-    }
-
-    // trovare la casa produttrice più ricorrente
-    public String casaRicorrente()
-    {
-        // estrazione di tutte le case produttrici
-        ArrayList<String> caseProd = estraiCaseProduttrici();
-        String casaRic = caseProd.get(0);
-        int maxOccorreze = contaOccorrenze(casaRic);
-        for ( int i = 1; i < caseProd.size(); i++ )
-        {
-            int occorrenze = contaOccorrenze(caseProd.get(i))
-            if ( occorrenze > maxOccorreze )
-            {
-                casaRic = caseProd.get(i);
-                maxOccorreze = occorrenze; 
-            }
-        }
-    }
-
-    private ArrayList<String> estraiCaseProduttrici(){
-
-        ArrayList<String> caseProd = new ArrayList<>();
-        String casa = a.getCasaProduttrice();
-        for (Automobile a: magazzino){
-            if ( !(caseProd.add(casa)) )
-                caseProd.add(casa);   
-        }
-        return caseProd;
-    }
-
-    private int contaOccorrenze(String casa)
-    {
-        int cont = 0; 
-        for ( Automobile a: magazzino){
-            if (a.getCasaProduttrice().equals(casa))
-                cont ++;
-        }
-        return cont;
-    }
-    
-    // trovare l'auto con cilindrata più bassa con il prezzo inferiore a 10.000 euri
-
-    public Automobile cilindrataMinima()
-    {
-        int cilindrataMin = Integer.MAX_VALUE;
-        Automobile aMin = null; 
-        for ( Automobile a: magazzino ){
-            if ( a.getPrezzo() < 10000 && (aMin == null || a.getCilindrata() < aMin.getCilindrata() ) )
-                aMin = a;
-                cilindrataMin = a.getCilindrata(); 
-        } 
-    return aMin; 
-    }
-
-    public Automobile cilindrataAlta()
-    {
-        float mediaPrezzi = sommaPrezzi()/magazzino.size(); 
-        Automobile ris = null;
-        for ( Automobile a: magazzino)
-            if ( a.getPrezzo() > mediaPrezzi && ( ris == null || a.getCilindrata() > ris.getCilindrata()))
-                ris = a; 
-        return ris;
-    }
-
-    //trovare le case produttrici che producono solo auto con un prezzo inferiore alla media dei prezzi 
-    //delle auto
-
-    public ArrayList caseEconomiche()
-    {
-        ArrayList<String> tutteCase = estraiCaseProduttrici;
-        float mediaPrezzi = sommaPrezzi()/magazzino.size();
-        ArrayList<String> caseEc = new ArrayList<>();
-
-        for ( String casa: tutteCase )
-            if ( soloPrezzoInferiore(casa, mediaPrezzi) )
-                ris.add(casa);
-        return ris;  
-    }
-
-    // ret true sse tutte le automoibili di quella casaProd hanno prezzo ok
-    private boolean soloPrezzoInferiore(String casa, float mediaPrezzi)
-    {
-        for ( Automobile a: magazzino )
-            if ( a.getCasaProduttrice().equals(casa) && a.getPrezzo >= mediaPrezzi )
-                return false;
-        return true;
-    }
-
-    public ArrayList<String> caseStessaCilindrata() 
-    {
-        ArrayList<String> tutteCase = estraiCaseProduttrici();
-        ArrayList<String> ris = new ArrayList(); 
-        for ( String casa: tutteCase )
-        {
-            if ( stessaCilindrata(casa) )
-                ris.add(casa);
-        }
-    }
-
-    private boolean stessaCilindrata(String casa)
-    {
-        ArrayList<Automobile> autoCasa = new ArrayList<>();
-        for  (Automobile a: magazzino)
-            if ( a.getCasaProduttrice().equals(casa))
-                autoCasa.add(a);
-        int cilindrata = autoCasa.get(0);
-        for ( int i = 1; i < autoCasa.size(); i++ )
-            if ( ! ( autoCasa.get(i).getCilindrata() != cilindrata ))
-                return false;
-        return true; 
-    }
-
-    private boolean stessaCilindrata2(String casa)
-    {
-        ArrayList<Integer> cilindrateCasa = new ArrayList<>();
-        for  (Automobile a: magazzino)
-            if ( a.getCasaProduttrice().equals(casa))
-                cilindrateCasa.add(a.getCilindrata());
-
-        int cilindrata = autoCasa.get(0);
-        for ( int i = 1; i < autoCasa.size(); i++ )
-            if ( cilindrateCasa.get(i) != cilindrata )
-                return false;
-        return true; 
-    }
-}
+ package concessionaria;
+ 
+ import java.util.*;
+ import terminale.*;
+ 
+ public class Concessionaria {
+     
+     private ArrayList<Automobile> magazzino;
+     
+     public Concessionaria() 
+     { 	magazzino = new ArrayList<>(); 
+     }
+     
+     // aggiungere un'auto al magazzino;
+     public void aggiungiAuto(Automobile a) 
+     {	magazzino.add(a);
+     }
+     
+     // rimuovere un'auto dal magazzino;
+     public void eliminaAuto(Automobile a) 
+     {	magazzino.remove(a);
+     }
+     
+     // stampare tutte le auto;
+     public void stampaAutoMagazzino() 
+     {	for(Automobile a: magazzino)
+             Terminale.stampa(a);
+     }
+     
+     // calcolare la somma dei prezzi di tutte le auto;
+     public float sommaPrezziAuto() 
+     {	float somma = 0;
+         for(Automobile a: magazzino)
+             somma += a.getPrezzo();
+         return somma;
+     }
+     
+     // trovare la casa produttrice pi� ricorrente;
+     public String casaPiuRicorrente() 
+     {	ArrayList<String> caseProduttrici = estraiCase();
+         String casaPiuRicorrente = caseProduttrici.get(0);
+         int maxOcc = occorrenzeCasa(casaPiuRicorrente);
+         for(int i=1; i<caseProduttrici.size(); i++) 
+         {	String casa = caseProduttrici.get(i);
+             int numOcc = occorrenzeCasa(casa);
+             if(numOcc > maxOcc) 
+             {	casaPiuRicorrente = casa;
+                 maxOcc = numOcc;
+             }
+         }
+         return casaPiuRicorrente;
+     }
+     
+     private ArrayList<String> estraiCase() 
+     {	ArrayList<String> ris = new ArrayList<>();
+         for(Automobile a: magazzino) 
+         {	String casa = a.getCasaProduttrice();
+             if(!ris.contains(casa))
+                 ris.add(casa);
+         }
+         return ris;
+     }
+     
+     private int occorrenzeCasa(String casa) 
+     {	int c = 0;
+         for(Automobile a: magazzino)
+             if(a.getCasaProduttrice().equals(casa)) 
+                 c++;
+         return c;
+     }
+     
+     // trovare l'auto con la cilindrata pi� bassa tra quelle con un prezzo inferiore a 10.000 euro;
+     public Automobile cilindrataBassa() 
+     {	Automobile ris = null;
+         for(Automobile a: magazzino) 
+         {	if(a.getPrezzo() < 10000 && ( ris==null || a.getCilindrata() < ris.getCilindrata() ))
+                 ris = a;
+         }
+         return ris;
+     }
+     
+     // trovare l'auto con la cilindrata pi� alta tra quelle con un prezzo superiore alla media dei prezzi delle auto;
+     public Automobile cilindrataAlta() 
+     {	float media = mediaPrezzi();
+         Automobile ris = null;
+         for(Automobile a: magazzino)
+             if(a.getPrezzo() > media && (ris==null || a.getCilindrata() > ris.getCilindrata()))
+                 ris = a;
+         return ris;
+     }
+     
+     private float mediaPrezzi() 
+     {	return sommaPrezziAuto()/magazzino.size();
+     }
+         
+     // trovare le case produttrici che producono solo auto con un prezzo inferiore alla media dei prezzi delle auto;
+     public ArrayList<String> caseEconomiche()
+     {	ArrayList<String> ris = new ArrayList<>();
+         float media = mediaPrezzi();
+         ArrayList<String> caseProd = estraiCase();
+         for(String casa: caseProd) 
+         if(tuttiPrezziInferiori(casa, media))
+                 ris.add(casa);
+         return ris;
+     }
+     
+     private boolean tuttiPrezziInferiori(String casa, float media) 
+     {	for(Automobile a: magazzino)
+             if(a.getCasaProduttrice().equals(casa) && a.getPrezzo()>=media)
+                 return false;
+         return true;
+     }
+     
+     // trovare le case produttrici che producono solo auto con la stessa cilindrata.
+     public ArrayList<String> caseStessaCilindrata() 
+     {	ArrayList<String> ris = new ArrayList<>();
+         ArrayList<String> caseProd = estraiCase();
+         for(String casa: caseProd)
+             if(tutteStessaCilindrata(casa))
+                 ris.add(casa);
+         return ris;
+     }
+     
+     private boolean tutteStessaCilindrata(String casaProd) 
+     {	ArrayList<Automobile> automobili = new ArrayList<>();
+         for(Automobile a: magazzino)
+             if(a.getCasaProduttrice().equals(casaProd))
+                 automobili.add(a);
+         int cil = automobili.get(0).getCilindrata();
+         for(int i=1; i<automobili.size(); i++)
+             if(automobili.get(i).getCilindrata() != cil)
+                 return false;
+         return true;
+     }
+ 
+ }
