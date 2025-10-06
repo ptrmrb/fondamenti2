9927ffc29d415547c73e9f857d2f0354105cc246
Pietro Mirabelli <pietromirabelli@MacBook-Air-di-Pietro.local>
2025-10-06 17:44:54 +0200

    Modifica Biglietto.java

--- lezioni/Biglietto.java
+++ lezioni/Biglietto.java
@@ -2,20 +2,20 @@ package lezioni;
 
 public class Biglietto {
 
-    private int numero;
+    private int num;
     private static int numeroBigliettiCreati = 0;
 
     public Biglietto()
     {
         //numero = numeroBigliettiCreati + 1;
-        this.numero = Biglietto.numeroBigliettiCreati + 1;
+        this.num = Biglietto.numeroBigliettiCreati + 1;
         Biglietto.numeroBigliettiCreati ++;
     }
 
     // da un metodo statico non posso fare riferimento ad attribiti non statici
 
     public String toString(){
-        return "Biglietto numero " + numero;
+        return "Biglietto numero " + num;
     }
     
     public static void reset(){
